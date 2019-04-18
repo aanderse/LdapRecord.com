62b236e9214fbdc2d32bd56706c1e312adb3b7be
Steve Bauman <steven_bauman@outlook.com>
2019-04-18 16:37:48 -0400

    Added logo to navbar.

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-light navbar-expand-lg bg-light border-bottom">
 	<div class="container">
-		<a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name')  }}</a>
+		<a class="navbar-brand" href="{{ url('/') }}">
+			<img src="{{ asset('assets/img/ldap-record.png') }}" width="150">
+		</a>
 
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-main" aria-controls="nav-main" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
