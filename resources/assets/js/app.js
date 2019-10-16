933beafa324582d97dc9592e7c64d39510921935
Steve Bauman <steven_bauman@outlook.com>
2019-10-15 20:57:48 -0400

    New asset build.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,28 +8,28 @@ window.jQuery = window.$ = require('jquery');
 require('bootstrap');
 
 // Add anchors to each doc heading.
-$('article h2, h3').each((index, element) => {
-    var anchor = $(element).html().toLowerCase()
+$('article h2, h3').each(function () {
+    var anchor = $(this).html().toLowerCase()
         .replace(/[^\w ]+/g,'')
         .replace(/ +/g,'-');
 
-    $(element).html('<a id="'+anchor+'" href="#'+anchor+'">' +
-        '<span class="anchor-tag">#</span> '+$(element).html()+'</a>'
+    $(this).html('<a id="'+anchor+'" href="#'+anchor+'">' +
+        '<span class="anchor-tag">#</span> '+$(this).html()+'</a>'
     );
 });
 
 // Wrap blockquote content in a div for flex-box.
-$('.docs blockquote').each((index, element) => {
-    $(element).wrap('<div></div>');
+$('.docs blockquote').each(function () {
+    $(this).html('<div>'+$(this).html()+'</div>');
 });
 
-$('table').each((index, element) => {
-    $(element).wrap('<div class="table-responsive"></div>');
+$('table').each(function () {
+  $(this).html('<div class="overflow-hidden">'+$(this).html()+'</div>');
 });
 
 // Parse blockquote content for flags.
-$('.docs blockquote p').each((index, element) => {
-    var str = $(element).html();
+$('.docs blockquote p').each(function() {
+    var str = $(this).html();
     var match = str.match(/\{(.*?)\}/);
 
     if (match) {
@@ -47,12 +47,12 @@ $('.docs blockquote p').each((index, element) => {
                 break;
         }
 
-        $(element).html(str.replace(/\{(.*?)\}/, ''));
-        $(element).parent().before('<div class="flag">'+ icon +'</div>');
+        $(this).html(str.replace(/\{(.*?)\}/, ''));
+        $(this).parent().before('<div class="flag">'+ icon +'</div>');
     }
 });
 
 // Set active classes in the side navigation links.
-$(".side-nav").find("a[href='"+window.location.pathname+"']").each((index, element) => {
-    $(element).addClass("active");
+$(".side-nav").find("a[href='"+window.location.pathname+"']").each(function() {
+    $(this).addClass("active");
 });
