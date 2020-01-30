b27d9332ea314d9f54083c19fa1423d6cf6cc8d4
Steve Bauman <steven_bauman@outlook.com>
2020-01-29 22:57:57 -0500

    Added conditional display of doc links

--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -2,6 +2,28 @@
 
 @section('nav')
     @include('_nav.menu', ['items' => $page->navigation])
+
+    <div class="block md:hidden">
+        <hr class="my-4"/>
+
+        <ul class="list-none my-0">
+            <li class="pl-4">
+                <a href="/docs/laravel/"
+                   class="nav-menu__item hover:text-blue-500"
+                >
+                    Laravel Documentation
+                </a>
+            </li>
+
+            <li class="pl-4">
+                <a href="https://github.com/DirectoryTree/LdapRecord"
+                   class="nav-menu__item hover:text-blue-500"
+                >
+                    Source Code
+                </a>
+            </li>
+        </ul>
+    </div>
 @endsection
 
 @section('footer')
--- source/_layouts/laravel-documentation.blade.php
+++ source/_layouts/laravel-documentation.blade.php
@@ -2,6 +2,28 @@
 
 @section('nav')
     @include('_nav.menu', ['items' => $page->laravelNavigation])
+
+    <div class="block sm:hidden">
+        <hr class="my-4"/>
+
+        <ul class="list-none my-0">
+            <li class="pl-4">
+                <a href="/docs/"
+                   class="nav-menu__item hover:text-blue-500"
+                >
+                    Core Documentation
+                </a>
+            </li>
+
+            <li class="pl-4">
+                <a href="https://github.com/DirectoryTree/LdapRecord-Laravel"
+                   class="nav-menu__item hover:text-blue-500"
+                >
+                    Source Code
+                </a>
+            </li>
+        </ul>
+    </div>
 @endsection
 
 @section('footer')
